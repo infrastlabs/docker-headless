d8ee00a06849b07401a12ccb2139f234d3975c91
sam <sam@allofword>
2021-07-03 15:45:27 +0800

    squash: locale02; use1; --- # 23.190,191,192
    use1
    
    try2
    
    LANG list
    
    # LANG=zh_CN.UTF-8: 格式> 语言_地区:编码
    
    TZ/LOCALE deep, sort_scripts
    
    chmod +x
    
    sdf
    
    spit TZ;  notes LANG1
    
    clipit: initRun confirm

--- DE/entry.sh
+++ DE/entry.sh
@@ -1,20 +1,42 @@
 
 #!/bin/bash
 
-export LANG=${LOC_LANG_COUNTRY}.${LOC_CODIFICATION} \
+# LOCALE_REF
+# https://manpages.debian.org/stretch/manpages/localedef.1.en.html
+# https://wiki.archlinux.org/title/locale_(简体中文)#LANGUAGE：后备区域设置
+
+# LOC_LANG_COUNTRY=fr_CA #dbg > LOC_LANG_AREA
+# LOC_CODIFICATION=UTF-8
+# LANG=zh_CN.UTF-8: 格式> 语言_地区:编码
+export LANG=${LOC_LANG_COUNTRY}.${LOC_CODIFICATION}
 export LANGUAGE=${LOC_LANG_COUNTRY}:zh
 env |grep "LOC\|LANG"
 
-localedef -i ${LOC_LANG_COUNTRY} -c -f ${LOC_CODIFICATION} -A /usr/share/locale/locale.alias ${LANG} \
-  && sed -i -e "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen \
-  && sed -i -e "s/# ${LANG} ${LOC_CODIFICATION}/${LANG} ${LOC_CODIFICATION}/" /etc/locale.gen \
-  && ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime && echo "${TZ}" >/etc/timezone \
-  && dpkg-reconfigure --frontend=noninteractive locales tzdata \
-  && locale-gen ${LANG} \
-  && /usr/sbin/update-locale LANG=${LANG}; \
-    # /etc/default/locale
-    echo "# " > /etc/default/locale; \
-    echo "LANG=${LANG}" >> /etc/default/locale; \
-    echo "LANGUAGE=${LANGUAGE}" >> /etc/default/locale;
+##localtime + timezone [check: date -R]#####
+# TZ="Asia/Shanghai" #Etc/GMT-9
+echo "${TZ}" >/etc/timezone
+ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
+# dpkg-reconfigure --frontend=noninteractive tzdata
+
+
+
+##[check: locale -a; locale -ck date_fmt]##################
+# localedef
+# gen: > /usr/lib/locale/C.UTF-8
+# localedef -c -f ${LOC_CODIFICATION} -i ${LOC_LANG_COUNTRY} -A /usr/share/locale/locale.alias ${LANG}
+
+# locale-gen < /etc/locale.gen
+sed -i -e "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
+sed -i -e "s/# ${LANG} ${LOC_CODIFICATION}/${LANG} ${LOC_CODIFICATION}/" /etc/locale.gen
+locale-gen ${LANG}
+# dpkg-reconfigure locales
+
+# /etc/default/locale; ###LANGUAGE=en_AU:en_GB:en
+# update-locale LANG=${LANG} LANGUAGE=xx AA=11;
+echo "# " > /etc/default/locale; \
+echo "LANG=${LANG}" >> /etc/default/locale; \
+echo "LANGUAGE=${LANGUAGE}" >> /etc/default/locale;
+########################################################
+
 
 exec supervisord -n
