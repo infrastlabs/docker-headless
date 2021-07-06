ab75daa155fca13ca9e7fd089db00b2ca1af0ebb
sam <sam@allofword>
2021-07-06 10:51:24 +0800

    squash: locale1 --- sdf
    Desktop/loc_anay/loc_select
    
    f locale1
    
    sdf
    
    f
    
    dbg: deb9 only
    
    locale1
    
    sync: lite locale

--- DE/entry.sh
+++ DE/entry.sh
@@ -4,39 +4,35 @@
 # LOCALE_REF
 # https://manpages.debian.org/stretch/manpages/localedef.1.en.html
 # https://wiki.archlinux.org/title/locale_(简体中文)#LANGUAGE：后备区域设置
-
-# LOC_LANG_COUNTRY=fr_CA #dbg > LOC_LANG_AREA
-# LOC_CODIFICATION=UTF-8
 # LANG=zh_CN.UTF-8: 格式> 语言_地区:编码
-export LANG=${LOC_LANG_COUNTRY}.${LOC_CODIFICATION}
-export LANGUAGE=${LOC_LANG_COUNTRY}:zh
-env |grep "LOC\|LANG"
 
-##localtime + timezone [check: date -R]#####
-# TZ="Asia/Shanghai" #Etc/GMT-9
+# TZ # TZ="Asia/Shanghai"
 echo "${TZ}" >/etc/timezone
 ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
-# dpkg-reconfigure --frontend=noninteractive tzdata
 
+function setLocale(){
+    env |grep "TZ\|^L="
+    # L="zh_CN" ##${L##*.} > zh_CN
+    # LANG="zh_CN.UTF-8" #lang_area.charset
+    charset=${L##*.}; test "$charset" == "$L" && charset="UTF-8" || echo "charset: $charset"
+    lang_area=${L%%.*}
+    export LANG=${lang_area}.${charset}
+    export LANGUAGE=${lang_area}:en #default> en
+    echo "====LANG: $LANG, LANGUAGE: $LANGUAGE=========================="
+    sleep 1
 
+    # LOCALE
+    sed -i -e "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
+    sed -i -e "s/# ${LANG} ${charset}/${LANG} ${charset}/" /etc/locale.gen
+    locale-gen ${LANG}
+    update-locale LANG=${LANG} LANGUAGE=${LANGUAGE}
+    # echo "# " > /etc/default/locale;
 
-##[check: locale -a; locale -ck date_fmt]##################
-# localedef
-# gen: > /usr/lib/locale/C.UTF-8
-# localedef -c -f ${LOC_CODIFICATION} -i ${LOC_LANG_COUNTRY} -A /usr/share/locale/locale.alias ${LANG}
-
-# locale-gen < /etc/locale.gen
-sed -i -e "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
-sed -i -e "s/# ${LANG} ${LOC_CODIFICATION}/${LANG} ${LOC_CODIFICATION}/" /etc/locale.gen
-locale-gen ${LANG}
-# dpkg-reconfigure locales
-
-# /etc/default/locale; ###LANGUAGE=en_AU:en_GB:en
-# update-locale LANG=${LANG} LANGUAGE=xx AA=11;
-echo "# " > /etc/default/locale; \
-echo "LANG=${LANG}" >> /etc/default/locale; \
-echo "LANGUAGE=${LANGUAGE}" >> /etc/default/locale;
-########################################################
-
+    echo "==[locale -a]===================================="
+    locale -a
+    cat /etc/default/locale
+    # sleep 2
+}
+test -z "$L" || setLocale
 
 exec supervisord -n
