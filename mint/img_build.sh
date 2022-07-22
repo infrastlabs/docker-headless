fa9524a118ee0279ceb6af960ea8e5fe51ce235f
sam@gemibook <huapox@126.com>
2022-07-22 19:56:01 +0800

    init mint

---
+++ mint/img_build.sh
@@ -0,0 +1,23 @@
+source /etc/profile
+export |grep DOCKER_REG
+repo=registry.cn-shenzhen.aliyuncs.com
+echo "${DOCKER_REGISTRY_PW_infrastSubUser2}" |docker login --username=${DOCKER_REGISTRY_USER_infrastSubUser2} --password-stdin $repo
+
+ns=infrastlabs
+# cache="--no-cache"
+# pull="--pull"
+
+# latest
+case "$1" in
+compile)
+    img="docker-headless:mint-compile"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.compile . 
+    docker push $repo/$ns/$img
+    ;; 
+
+*)
+    img="docker-headless:mint"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile . 
+    docker push $repo/$ns/$img    
+    ;;
+esac
