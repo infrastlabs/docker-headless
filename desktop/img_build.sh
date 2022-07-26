ca09c1c1aa57a85a260d943f687c7ce02f702a64
sam@gemibook <huapox@126.com>
2022-07-26 22:13:41 +0800

    +build

---
+++ desktop/img_build.sh
@@ -0,0 +1,49 @@
+source /etc/profile
+export |grep DOCKER_REG
+repo=registry.cn-shenzhen.aliyuncs.com
+echo "${DOCKER_REGISTRY_PW_infrastSubUser2}" |docker login --username=${DOCKER_REGISTRY_USER_infrastSubUser2} --password-stdin $repo
+
+ns=infrastlabs
+# cache="--no-cache"
+# pull="--pull"
+
+# latest
+case "$1" in
+cinna)
+    img="docker-headless:cinna"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.cinna . 
+    docker push $repo/$ns/$img
+    ;;   
+cmate)
+    img="docker-headless:cmate"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.cmate . 
+    docker push $repo/$ns/$img
+    ;;       
+cxfce)
+    img="docker-headless:cxfce"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.cxfce . 
+    docker push $repo/$ns/$img
+    ;;  
+plas)
+    img="docker-headless:plas"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.plasma . 
+    docker push $repo/$ns/$img
+    ;;      
+
+# v1.5 ex
+neon)
+    img="docker-headless:neon"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.neon . 
+    docker push $repo/$ns/$img
+    ;;   
+elm)
+    img="docker-headless:elm"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.elm . 
+    docker push $repo/$ns/$img
+    ;;   
+*)
+    img="docker-headless:gnome"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.gnome . 
+    docker push $repo/$ns/$img    
+    ;;
+esac
