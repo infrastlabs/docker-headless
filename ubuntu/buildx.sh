09916b4f2e0423c40ae3f0126d0a22b450f5eaa6
sam@gemibook <huapox@126.com>
2022-07-30 22:43:31 +0800

    sq: +arm64 --- # vncpasswd: tigervnc-common
    v3; repo
    
    sdf
    
    infrastlabs/docker-headless:mint-compile-arm as Xvnc
    
    case
    
    up
    
    cp init arm
    
    repoHub=docker.io
            echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTRY_USER_dockerhub} --password-stdin $repoHub
    
    +buildx.sh
    
    up arm
    
    up arm
    
    up t1
    
    t1
    
    +pnmixer, diodon

---
+++ ubuntu/buildx.sh
@@ -0,0 +1,43 @@
+
+
+
+source /etc/profile
+export |grep DOCKER_REG
+repo=registry.cn-shenzhen.aliyuncs.com
+echo "${DOCKER_REGISTRY_PW_infrastSubUser2}" |docker login --username=${DOCKER_REGISTRY_USER_infrastSubUser2} --password-stdin $repo
+
+repoHub=docker.io
+echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTRY_USER_dockerhub} --password-stdin $repoHub
+        
+ns=infrastlabs
+# cache="--no-cache"
+# pull="--pull"
+ver=v3 # latest
+case "$1" in
+compile)
+    # vncver=1.12.0 ## udomain > jaist
+    # file=tigervnc-${vncver}.x86_64.tar.gz
+    # test -s "$file" || curl -fSL -k -O https://jaist.dl.sourceforge.net/project/tigervnc/stable/${ver}/$file
+    # 
+    img="docker-headless:mint-compile-arm"
+    # --platform linux/amd64,linux/arm64
+    # repo=registry-1.docker.io
+    # plat="--platform linux/arm64"
+    plat="--platform linux/amd64,linux/arm64"
+    # multi:  docker buildx create --use mybuilder; moby/buildkit:buildx-stable-1
+    # docker buildx build --platform linux/amd64,linux/arm64 --push -t $ns/$img -f src/Dockerfile.compile.t1 . 
+    docker buildx build $plat --push -t $ns/$img -f src/Dockerfile.compile.arm . 
+    # docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.compile . 
+    # docker push $repo/$ns/$img
+    ;;
+*)
+    repo=registry-1.docker.io
+    img="docker-headless:ubt-$ver-arm"
+    # --cache-from $repo/$ns/$img 
+    plat="--platform linux/amd64,linux/arm64"
+    # docker build $cache $pull -t $repo/$ns/$img --build-arg FULL=/.. -f src/Dockerfile.arm . 
+    # --build-arg FULL=/..
+    docker  buildx build $plat --push -t $repo/$ns/$img -f src/Dockerfile.arm . 
+    docker push $repo/$ns/$img    
+    ;;
+esac
