aa82c7de1614c287d16df777b33b7304e00dd8ee
sam#gemmi-win10 <sam@china.gz>
2022-08-18 20:56:32 +0800

    core: build> update; ver=v5 ---
    core-compile
    
    core: build +dbg
    
    core: up build
    
    img: -conf
    
    conf > attach
    
    core: build> drop dbg
    
    core: build> core-v4, base-v4
    
    core: build > # 关联子项: ubt20, gnome,deb10
    ---
    
    core: cd ../deb10; sh img_build.sh
    
    core: build> drop usr
    
    core: build> && echo -e "\n\nimg: $img"
    
    core: build > fix img2
    
    core: ver=v5 # only: base-v5, base-v5-slim

--- ubt-core/img_build.sh
+++ ubt-core/img_build.sh
@@ -7,14 +7,10 @@ ns=infrastlabs
 # cache="--no-cache"
 # pull="--pull"
 
-ver=v4 # latest
+ver=v5 # only: base-v5, base-v5-slim
 case "$1" in
 compile)
-    vncver=1.12.0 ## udomain > jaist
-    file=tigervnc-${vncver}.x86_64.tar.gz
-    test -s "$file" || curl -fSL -k -O https://jaist.dl.sourceforge.net/project/tigervnc/stable/${ver}/$file
-    # 
-    img="docker-headless:mint-compile"
+    img="docker-headless:core-compile"
     docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.compile . 
     docker push $repo/$ns/$img
     ;; 
@@ -28,22 +24,42 @@ compile)
 #     docker build $cache $pull -t $repo/$ns/$img -f src/arm.Dockerfile.tiger-v21 . 
 #     docker push $repo/$ns/$img
 #     ;;     
-usr)
-    img="docker-headless:ubt-$ver-usr"
-    # --cache-from $repo/$ns/$img 
-    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.usrclear . 
+hub)
+    repoHub=docker.io
+    echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTRY_USER_dockerhub} --password-stdin $repoHub
+    # SLIM
+    img="docker-headless:core" && echo -e "\n\nimg: $img"
+    docker tag $repo/$ns/$img $ns/$img; docker push $ns/$img
+    # 
+    img="docker-headless:base-$ver" && echo -e "\n\nimg: $img"
+    docker tag $repo/$ns/$img $ns/$img; docker push $ns/$img
+    img="docker-headless:base-$ver-slim" && echo -e "\n\nimg: $img"
+    docker tag $repo/$ns/$img $ns/$img; docker push $ns/$img
+    ;;    
+slim)
+    img="docker-headless:base-$ver-slim"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.base . 
     docker push $repo/$ns/$img    
     ;;
-slim)
-    img="docker-headless:ubt-$ver-slim"
+base)
+    img="docker-headless:base-$ver"
     # --cache-from $repo/$ns/$img 
-    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile . 
-    docker push $repo/$ns/$img    
+    docker build $cache $pull -t $repo/$ns/$img --build-arg FULL=/.. -f src/Dockerfile.base . 
+    docker push $repo/$ns/$img 
     ;;
 *)
-    img="docker-headless:ubt-$ver"
-    # --cache-from $repo/$ns/$img 
-    docker build $cache $pull -t $repo/$ns/$img --build-arg FULL=/.. -f src/Dockerfile . 
-    docker push $repo/$ns/$img    
+    vncVer=1.10.1 #1.12.0 ## udomain > jaist
+    file=tigervnc-${vncVer}.x86_64.tar.gz
+    test -s "$file" || curl -fSL -k -O https://jaist.dl.sourceforge.net/project/tigervnc/stable/${vncVer}/$file
+    # 
+    img="docker-headless:core" && echo -e "\n\nimg: $img"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile . 
+    # docker push $repo/$ns/$img    
+
+    # 关联子项: ubt20, gnome
+    export IMG_PUSH=false
+    cd ../deb10; sh img_build.sh
+    cd ../ubt-custom; sh img_build.sh
+    cd ../ubt-desktop; sh img_build.sh
     ;;
 esac
