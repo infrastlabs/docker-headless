be65eabd76fe945bf6486b6d5dadc13bce965a6d
sam#gemmi-win10 <sam@china.gz>
2022-08-20 13:48:05 +0800

    cust: build > +ibus && echo -e "\n\nimg: $img"；clear tags
    cust: build > fix img2

--- ubt-custom/img_build.sh
+++ ubt-custom/img_build.sh
@@ -7,26 +7,30 @@ ns=infrastlabs
 # cache="--no-cache"
 # pull="--pull"
 
-ver=v6 # latest
+# ver=v6 # latest
 case "$1" in
 hub)
     repoHub=docker.io
     echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTRY_USER_dockerhub} --password-stdin $repoHub
     # SLIM
-    img="docker-headless:ubt20-$ver" && echo -e "\n\nimg: $img"
+    img="docker-headless:latest" && echo -e "\n\nimg: $img"
     docker tag $repo/$ns/$img $ns/$img; docker push $ns/$img
-    img2=docker-headless:ubt20
-    docker tag $repo/$ns/$img $ns/$img2; docker push $ns/$img2
+    # 
+    img="docker-headless:sogou"
+    docker tag $repo/$ns/$img $ns/$img; docker push $ns/$img
     ;;
-*)
+sogou)
     file=sogoupinyin_4.0.1.2800_x86_64.deb
     test -s "$file" || curl -O https://ime.sogouimecdn.com/202208081957/72cdf6131e248391c874cb05d0e8401b/dl/gzindex/1656597217/$file
-    img="docker-headless:ubt20-$ver"
+    # 
+    img="docker-headless:sogou"
+    docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile.sogou . 
+    docker push $repo/$ns/$img    
+    ;;    
+*)
+    img="docker-headless:latest" && echo -e "\n\nimg: $img"
     # --cache-from $repo/$ns/$img 
     docker build $cache $pull -t $repo/$ns/$img -f src/Dockerfile . 
     test "false" == "$IMG_PUSH" || docker push $repo/$ns/$img    
-    img2=docker-headless:ubt20
-    docker tag $repo/$ns/$img $repo/$ns/$img2; 
-    test "false" == "$IMG_PUSH" || docker push $repo/$ns/$img2
     ;;
 esac
