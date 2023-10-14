9d057f4afc45624e5b0cdf92a1821b312c686f59
sam#gemmi-win10 <sam@china.gz>
2023-10-15 04:02:39 +0800

    +src/xrdp: keymap, startwm.sh -- src xrdp certs x2
    +src/etc/xrdp/startwm.sh

---
+++ deb12/src/etc/xrdp/keymap.sh
@@ -0,0 +1,25 @@
+ #!/bin/bash
+
+# https://manpages.debian.org/testing/xrdp/xrdp-genkeymap.8.en.html
+# 00000409 en-us US English
+# in DISPLAY env
+/usr/local/xrdp/bin/xrdp-genkeymap \
+/etc/xrdp/km-00000407.ini \
+/etc/xrdp/km-00000409.ini \
+/etc/xrdp/km-0000040a.ini \
+/etc/xrdp/km-0000040b.ini \
+/etc/xrdp/km-0000040c.ini \
+/etc/xrdp/km-00000410.ini \
+/etc/xrdp/km-00000411.ini \
+/etc/xrdp/km-00000412.ini \
+/etc/xrdp/km-00000414.ini \
+/etc/xrdp/km-00000415.ini \
+/etc/xrdp/km-00000416.ini \
+/etc/xrdp/km-00000419.ini \
+/etc/xrdp/km-0000041d.ini \
+/etc/xrdp/km-00000807.ini \
+/etc/xrdp/km-00000809.ini \
+/etc/xrdp/km-0000080c.ini \
+/etc/xrdp/km-00000813.ini \
+/etc/xrdp/km-00000816.ini \
+/etc/xrdp/km-0000100c.ini
