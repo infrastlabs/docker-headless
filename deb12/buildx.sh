2736d721ca6f485bfa222ad46a73bc6953ebd634
sam#gemmi-win10 <sam@china.gz>
2023-10-15 00:20:28 +0800

    sq: update, +cust -- x64 only
    cp up Dockerfile.custom
    
    ali's reg
    
    fix
    
    dockerfile: core, fix theme ins
    
    # deb12: clipit 88M;
    
    diodon 6597 kB
    
    base# deb12.out: language-pack-gnome-zh-hans
    
    base  x11-xkb-utils xkb-data
    
    xkb ref # https://github.com/jlesage/docker-baseimage-gui/blob/master/src/tigervnc/build.sh
    
    compile2: gen entry.sh
    
    x11-xkb-utils libopus0 \
    
    COPY --from=stage-tiger /usr/share/X11/xkb/ /usr/share/X11/xkb/
    
    #    xkb-data:/usr/share/X11/xkb/
    
    try notes deps
    
    a
    
    s
    
    left needed
    
    vncpasswd
    
    back v1.12.0
    
    buildx xrdp no-compile
    
    #1.20.7.tar.bz2 >21.1.7.tar.gz;
    
    full plat x64
    
    fix
    
    compile2: try tiger 1.13.1
    
    compile2: xorg 21.1.7
    
    compile2: 21.1.7.tar.gz
    
    notes patch
    
    cpl2
    
    base: # TEMP@deb12
    
    base: up repo
    
    base non-free-firmware
    
    DOCKERFILE: RUN apt.sh libjpeg62-turbo openssl
    
    up
    
    up repo
    
    #libLLVM-12.so.1@ubt2004 #libLLVM-15.so.1 #115M
      rm -f /usr/lib/*-linux-gnu/libLLVM*
    
    dockerfile: x11-xkb-utils xkb-data
    
    buildx: compile2 src #1.20.7 >21.1.7; 1.12.0> 1.13.1
            ver=1.13.1
    
    buildx 21.1.7
    
    --enable-static \
    
    binutils-x86-64-linux-gnu
    
    sd
    
    openssl
    
    try static
    
    buildx # cache ##support multi-stage? (无用)
    
    buildx compile2: plat x3
    
    compile2 # cache ##support multi-stage?
    
    $cache
    
    buildx full compile2
    
    # RUN /rootfs/usr/local/tigervnc/bin/Xvnc -version
    
    libcrypt1
    
    --with-cc-opt="-static" \
            --with-ld-opt="-static" \
            --with-cpu-opt="generic" \
    
    compile2: patch before make
    
    tiger common: make
    
    fix
    
    find /build/tigervnc-1.12.0/ |egrep Xvnc; \
    
    # deb12.out: libjpeg8@ubt2004 > libjpeg62-turbo
    
    compile2: no tigervnc/bin/Xvnc; # xorg-dev
    
    open #make vncpasswd, vncconfig, vncviewver, ...
    
    compile2: #   build the normal parts of TigerVNC first.
    
    +libdrm-dev libxfont-dev mesa-common-dev \ @apt.sh
    
    sort args; --enable-glx \
    
    /build/tigervnc-1.12.0/CMakeFiles/CMakeError.log
    
    fix
    
    s
    
    s
    
    RUN apt update; apt install -y \; try libxcb-dri3-0
    
    sort order
    
    # TODO: 版本不一定对得上..v1.12.0; 手动ct-compileImg: 未执行本步;
    
    base: notes # chansrv-err: libfdk-aac.so.2: cannot open shared object file: No such file
    
    only tiger
    
    buillx plat amd64

--- deb12/buildx.sh
+++ deb12/buildx.sh
@@ -28,15 +28,22 @@ compile2)
         ver=0.9.23 #0.9.16 (0.9.23.1 @23.10.14)
         file=xrdp-${ver}.tar.gz; test -s $file || curl -k -O -fSL https://ghproxy.com/https://github.com/neutrinolabs/xrdp/releases/download/v${ver}/$file
         # tiger
+        #1.20.7.tar.bz2 >21.1.7.tar.gz;    1.12.0> 1.13.1
+        ver=1.12.0 #1.13.1
         file=xorg-server-1.20.7.tar.bz2; test -s $file || curl -k -O -fSL https://www.x.org/pub/individual/xserver/$file #6.1M
-        file=tigervnc-1.12.0.tar.gz; test -s $file || curl -k -O -fSL https://ghproxy.com/https://github.com/TigerVNC/tigervnc/archive/v1.12.0/$file #1.5M
+        file=tigervnc-${ver}.tar.gz; test -s $file || curl -k -O -fSL https://ghproxy.com/https://github.com/TigerVNC/tigervnc/archive/v${ver}/$file #1.5M
         # curl -O -fsSL https://www.linuxfromscratch.org/patches/blfs/svn/tigervnc-1.12.0-configuration_fixes-1.patch
     cd $old;
     #  
     repo=registry-1.docker.io
     img="docker-headless:deb12-compile2"
+    # cache ##support multi-stage? (无用)
+    # ali="registry.cn-shenzhen.aliyuncs.com"
+    # cimg="docker-headless-cache:deb12-compile2"
+    # cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
+
     plat="--platform linux/amd64,linux/arm64,linux/arm" #,linux/arm
-    # plat="--platform linux/amd64"
+    plat="--platform linux/amd64"
     args="--provenance=false"
     args="""
     --provenance=false 
@@ -45,7 +52,7 @@ compile2)
     --build-arg COMPILE_TIGER=yes
     """
     # --network=host: docker buildx create --use --name mybuilder2 --buildkitd-flags '--allow-insecure-entitlement network.host'
-    docker buildx build $plat $args --push -t $ns/$img -f src/Dockerfile.compile2 . 
+    docker buildx build $cache $plat $args --push -t $ns/$img -f src/Dockerfile.compile2 . 
     ;;
 
 # +slim,base (docker-headless,weskit; use docker-headless's)
@@ -59,7 +66,7 @@ slim)
     cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
     
     plat="--platform linux/amd64,linux/arm64,linux/arm" ##linux/arm> linux/arm/v7
-    # plat="--platform linux/amd64"
+    plat="--platform linux/amd64"
     args="--provenance=false --build-arg FULL="
     docker buildx build $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.base . 
     ;;
@@ -72,10 +79,23 @@ base)
     cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
 
     plat="--platform linux/amd64,linux/arm64,linux/arm"
-    # plat="--platform linux/amd64"
+    plat="--platform linux/amd64"
     args="--provenance=false --build-arg FULL=/.."
     docker buildx build $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.base . 
     ;;   
+cust)
+    repo=registry-1.docker.io
+    img="docker-headless:deb12"
+    # cache
+    ali="registry.cn-shenzhen.aliyuncs.com"
+    cimg="docker-headless-cache:deb12"
+    cache="--cache-from type=registry,ref=$ali/$ns/$cimg --cache-to type=registry,ref=$ali/$ns/$cimg"
+
+    plat="--platform linux/amd64,linux/arm64,linux/arm"
+    plat="--platform linux/amd64"
+    args="--provenance=false --build-arg FULL=/.."
+    docker buildx build $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.custom . 
+    ;;       
 *)
     repo=registry-1.docker.io
     repo=registry.cn-shenzhen.aliyuncs.com #@ali's repo
